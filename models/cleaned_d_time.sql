8f98d425c99bd136759c289a5cc40876e6fcfc32
TIAGO HECK <tiagobratzheck@hotmail.com>
2023-09-18 23:34:40 +0000

    class 5

---
+++ models/cleaned_d_time.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        materialized='ephemeral'
+    )
+}}
+
+select
+    time_id,
+    {% if target.type == "postgres" %} action_timestamp::timestamp
+    {% elif target.type == "bigquery" %} timestamp(action_timestamp)
+    {% else %} action_timestamp
+    {% endif %} as action_timestamp_
+from {{source('postgres', 'd_time')}}
--- models/d_time.sql
+++ models/d_time.sql
@@ -1,15 +1,5 @@
 {{config(materialized='table', tags=['dim'])}}
 
-with
-    cleaned_d_time as (
-        select
-            time_id,
-            {% if target.type == "postgres" %} action_timestamp::timestamp
-            {% elif target.type == "bigquery" %} timestamp(action_timestamp)
-            {% else %} action_timestamp
-            {% endif %} as action_timestamp_
-        from {{source('postgres', 'd_time')}}
-    )
 
 select
     time_id,
@@ -18,5 +8,5 @@ select
         EXTRACT({{ part | upper }} FROM action_timestamp_) as action_{{ part }},
     {% endfor %} 
 
-from cleaned_d_time
+from {{ref('cleaned_d_time')}}
 {{limit_lines_dev()}}
